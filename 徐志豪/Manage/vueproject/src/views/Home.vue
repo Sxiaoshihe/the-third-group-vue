744274fe4000784a80f86e4c46718b93a76273b8
不会诗的徐志摩 <68491636+xzh-China@users.noreply.github.com>
2020-09-14 09:20:45 +0800

    Add files via upload

---
+++ 徐志豪/Manage/vueproject/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+    <el-footer>Footer</el-footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
+<style scoped>
+</style>
